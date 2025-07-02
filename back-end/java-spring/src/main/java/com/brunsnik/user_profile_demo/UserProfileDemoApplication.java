3b456953f89682b472ba5a34fe17031c880467c0
Peter Berry <pwcberry@gmail.com>
2025-07-02 14:50:08 +1000

    Initial commit for "java-spring" back-end

---
+++ back-end/java-spring/src/main/java/com/brunsnik/user_profile_demo/UserProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.brunsnik.user_profile_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileDemoApplication.class, args);
+	}
+
+}
---
+++ back-end/java-spring/src/test/java/com/brunsnik/user_profile_demo/UserProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunsnik.user_profile_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
