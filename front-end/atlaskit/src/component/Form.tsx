b307600ce4825452f46cd595f18cc0075da66f1f
Peter Berry <pwcberry@gmail.com>
2025-07-15 06:17:24 +1000

    Set up button CSS

--- front-end/atlaskit/src/component/Form.tsx
+++ front-end/atlaskit/src/component/Form.tsx
@@ -5,40 +5,40 @@ import type { NavigateFunction } from "react-router";
 import type { FormAction } from "../types";
 
 interface FormProps {
-    action: FormAction;
-    children: ReactNode;
+  action: FormAction;
+  children: ReactNode;
 }
 
 function handleSubmit(action: FormAction, navigate: NavigateFunction) {
-    return async function (formData: FormData) {
-        await action(formData);
-        navigate("/");
-    };
+  return async function (formData: FormData) {
+    await action(formData);
+    navigate("/");
+  };
 }
 
 function SubmitButton() {
-    const { pending } = useFormStatus();
-    return (
-        <button type="submit" className="button button--primary" disabled={pending}>
-            Save and exit
-        </button>
-    );
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" className="button button__primary" disabled={pending}>
+      Save and exit
+    </button>
+  );
 }
 
 function Form({ action, children }: FormProps) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <form action={handleSubmit(action, navigate)}>
-            {children}
-            <div className="form__footer">
-                <button type="button" className="button button--secondary" onClick={() => navigate("/")}>
-                    Cancel
-                </button>
-                <SubmitButton />
-            </div>
-        </form>
-    );
+  return (
+    <form action={handleSubmit(action, navigate)}>
+      {children}
+      <footer className="form__footer">
+        <button type="button" className="button button__subtle" onClick={() => navigate("/")}>
+          Cancel
+        </button>
+        <SubmitButton />
+      </footer>
+    </form>
+  );
 }
 
 export default Form;
