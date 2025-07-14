074d54fb37a4e2fafe19e468bc321c95ee9c7559
Peter Berry <pwcberry@gmail.com>
2025-07-15 06:44:26 +1000

    Set up field and input CSS

--- front-end/atlaskit/src/page/AddressHistory.tsx
+++ front-end/atlaskit/src/page/AddressHistory.tsx
@@ -1,64 +1,64 @@
 import Form from "../component/Form.tsx";
 
 async function translate(formData: FormData) {
-    const fullAddress = formData.get("fullAddress");
-    const startDate = {
-        month: formData.get("month"),
-        year: formData.get("year"),
-    };
-    return {
-        json() {
-            return {
-                fullAddress,
-                startDate,
-                endDate: null,
-            };
-        },
-    };
+  const fullAddress = formData.get("fullAddress");
+  const startDate = {
+    month: formData.get("month"),
+    year: formData.get("year"),
+  };
+  return {
+    json() {
+      return {
+        fullAddress,
+        startDate,
+        endDate: null,
+      };
+    },
+  };
 }
 
 async function submitForm(formData: FormData) {
-    console.log("Address submitted");
-    const response = await translate(formData);
-    console.log(response.json());
+  console.log("Address submitted");
+  const response = await translate(formData);
+  console.log(response.json());
 }
 
 function AddressHistory() {
-    return (
-        <>
-            <h1>Current address</h1>
-            <Form action={submitForm}>
-                <fieldset>
-                    <div>
-                        <label htmlFor="">Address</label>
-                        <input type="text" name="fullAddress" />
-                    </div>
-                    <div>
-                        <label htmlFor="">When did you move in?</label>
-                        <select name="month">
-                            <option value="*">Month</option>
-                            <option value="January">January</option>
-                        </select>
-                        <select name="year">
-                            <option value="*">Year</option>
-                            <option value="2025">2025</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="">Do you own the property?</label>
-                        <label>
-                            <input type="radio" name="homeOwner" value="true" />
-                            Yes
-                        </label>
-                        <label>
-                            <input type="radio" name="homeOwner" value="false" />
-                            No
-                        </label>
-                    </div>
-                </fieldset>
-            </Form>
-        </>
-    );
+  return (
+    <>
+      <h1>Current address</h1>
+      <Form action={submitForm}>
+        <fieldset>
+          <div className="field-group">
+            <label htmlFor="">Address</label>
+            <input type="text" name="fullAddress" className="field-text" />
+          </div>
+          <div className="field-group">
+            <label htmlFor="">When did you move in?</label>
+            <select name="month" className="field-select">
+              <option value="*">Month</option>
+              <option value="January">January</option>
+            </select>
+            <select name="year" className="field-select">
+              <option value="*">Year</option>
+              <option value="2025">2025</option>
+            </select>
+          </div>
+          <div className="field-group">
+            <label htmlFor="">Do you own the property?</label>
+            <label>
+              <input type="radio" name="homeOwner" value="true" />
+              Yes
+            </label>
+            <label>
+              <input type="radio" name="homeOwner" value="false" />
+              No
+            </label>
+          </div>
+        </fieldset>
+      </Form>
+    </>
+  );
 }
 
 export default AddressHistory;
--- front-end/atlaskit/src/page/GovernmentIdentity.tsx
+++ front-end/atlaskit/src/page/GovernmentIdentity.tsx
@@ -1,34 +1,34 @@
 import Form from "../component/Form.tsx";
 
 async function translate(formData: FormData) {
-    return {
-        json() {
-            return {
-                identificationValue: formData.get("identificationValue"),
-            };
-        },
-    };
+  return {
+    json() {
+      return {
+        identificationValue: formData.get("identificationValue"),
+      };
+    },
+  };
 }
 
 async function submitForm(formData: FormData) {
-    console.log("Government identity submitted");
-    const response = await translate(formData);
-    console.log(response.json());
+  console.log("Government identity submitted");
+  const response = await translate(formData);
+  console.log(response.json());
 }
 
 function GovernmentIdentity() {
-    return (
-        <>
-            <h1>Tax File Number</h1>
-            <Form action={submitForm}>
-                <fieldset>
-                    <div>
-                        <p>What is your tax file number?</p>
-                        <input type="text" name="identificationValue" />
-                    </div>
-                </fieldset>
-            </Form>
-        </>
-    );
+  return (
+    <>
+      <h1>Tax File Number</h1>
+      <Form action={submitForm}>
+        <fieldset>
+          <div className="field-group">
+            <label>What is your tax file number?</label>
+            <input type="text" name="identificationValue" />
+          </div>
+        </fieldset>
+      </Form>
+    </>
+  );
 }
 export default GovernmentIdentity;
